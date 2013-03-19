682c6a7f854cbd71ce2ea63921f42dbda3791e1d
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-03-19 22:23:15 +0200

    Implement ring buffer to support modifiable post trig count
    This gives the possibility to keep the log of what have happened
    before the trigger instead of after the trigger.
    Default is to save 32 samples after trigger and 1024-32 samples
    before.
    Writing to wb address 0x4 change the post trigger count.

--- rtl/verilog/tracer.v
+++ rtl/verilog/tracer.v
@@ -8,6 +8,8 @@
  *
  * WB interface:
  * Write Address 0x0000, Arm the trigger
+ * Write Address 0x0004, Set post trigger count (default = 32)
+ *
  * Read Address  0x0000 - 0x03ff, Read trig0 trace log
  * Read Address  0x0400 - 0x07ff, Read data0 trace log
  * Read Address  0x0800 - 0x0bff, Read data1 trace log
@@ -47,6 +49,7 @@ module tracer(
    reg [31:0] 			 data1_rd;
    reg [31:0] 			 data2_rd;
    reg 				 new_trig;
+   reg [9:0] 			 post_trig_done_cnt;
    // Read
    assign wb_dat_o = (wb_adr_i[13:12] == 2'b00) ? trig0_rd :
                      (wb_adr_i[13:12] == 2'b01) ? data0_rd :
@@ -57,13 +60,17 @@ module tracer(
    // Write
    always @(posedge wb_clk_i) begin
       new_trig <= 0;
-      if (wb_rst_i)
-	trigger <= 32'h00000000;
-      else if (wb_stb_i & wb_cyc_i & wb_we_i)
-	if (wb_adr_i == 0) begin
-	   trigger  <= wb_dat_i;
-	   new_trig <= 1;
-	end
+      if (wb_rst_i) begin
+	 trigger <= 32'h00000000;
+	 post_trig_done_cnt <= 10'd32;
+      end else if (wb_stb_i & wb_cyc_i & wb_we_i) begin
+	 if (wb_adr_i == 0) begin
+	    trigger  <= wb_dat_i;
+	    new_trig <= 1;
+         end else if (wb_adr_i == 4) begin
+	    post_trig_done_cnt <= wb_dat_i[9:0];
+	 end
+      end
    end
 
    // Ack generation
@@ -82,61 +89,64 @@ module tracer(
    // Trigger
    //---------------------------------------------------------------------------
    reg [11:2] mem_pos;
-   reg 	      running;
+   reg [11:2] trig_pos;
+   reg [9:0]  post_trig_cnt;
+   reg        trig_hit;
    reg        done;
 
    always @(posedge wb_clk_i)
-     if (wb_rst_i | new_trig)
-       running <= 0;
-     else if (trig0_i == trigger)
-       running <= 1;
+     if (wb_rst_i | new_trig) begin
+	trig_pos <= 0;
+	trig_hit <= 0;
+     end else if (trig0_i == trigger & !trig_hit) begin
+	trig_pos <= mem_pos + 1;
+	trig_hit <= 1;
+     end
 
    always @(posedge wb_clk_i)
      if (wb_rst_i | new_trig)
+       post_trig_cnt <= 0;
+     else if (trig_hit & !done)
+       post_trig_cnt <= post_trig_cnt + 1;
+
+   always @(posedge wb_clk_i)
+     if (wb_rst_i)
        mem_pos <= 0;
-     else if (running & !(&mem_pos))
+     else
        mem_pos <= mem_pos + 1;
 
    always @(posedge wb_clk_i)
      if (wb_rst_i | new_trig)
        done <= 0;
-     else if (&mem_pos)
+     else if (post_trig_cnt == post_trig_done_cnt)
        done <= 1;
 
    //---------------------------------------------------------------------------
    // Logging logic (Block RAM's)
    //---------------------------------------------------------------------------
-   reg [31:0] trig0_mem[1023:0];
-   reg [31:0] data0_mem[1023:0];
-   reg [31:0] data1_mem[1023:0];
-   reg [31:0] data2_mem[1023:0];
-   reg [31:0] trig0_q;
-   reg [31:0] data0_q;
-   reg [31:0] data1_q;
-   reg [31:0] data2_q;
-   wire       wr_en;
+   reg  [31:0] trig0_mem[1023:0];
+   reg [31:0]  data0_mem[1023:0];
+   reg [31:0]  data1_mem[1023:0];
+   reg [31:0]  data2_mem[1023:0];
+   wire        wr_en;
    wire [11:2] wr_addr;
+   wire [11:2] rd_addr;
 
    assign wr_addr = mem_pos;
-   assign wr_en   = running & !done;
-
-   always @(posedge wb_clk_i) begin
-      trig0_q <= trig0_i;
-      data0_q <= data0_i;
-      data1_q <= data1_i;
-      data2_q <= data2_i;
-   end
+   assign rd_addr = wb_adr_i[11:2] +
+		    ((trig_pos + post_trig_done_cnt) - 10'd1023);
+   assign wr_en   = !done;
 
    always @(posedge wb_clk_i) begin
       if (wr_en) begin
-	 trig0_mem[wr_addr] <= trig0_q;
-	 data0_mem[wr_addr] <= data0_q;
-	 data1_mem[wr_addr] <= data1_q;
-	 data2_mem[wr_addr] <= data2_q;
+	 trig0_mem[wr_addr] <= trig0_i;
+	 data0_mem[wr_addr] <= data0_i;
+	 data1_mem[wr_addr] <= data1_i;
+	 data2_mem[wr_addr] <= data2_i;
       end
-      trig0_rd <= trig0_mem[wb_adr_i[11:2]];
-      data0_rd <= data0_mem[wb_adr_i[11:2]];
-      data1_rd <= data1_mem[wb_adr_i[11:2]];
-      data2_rd <= data2_mem[wb_adr_i[11:2]];
+      trig0_rd <= trig0_mem[rd_addr];
+      data0_rd <= data0_mem[rd_addr];
+      data1_rd <= data1_mem[rd_addr];
+      data2_rd <= data2_mem[rd_addr];
    end
 endmodule
